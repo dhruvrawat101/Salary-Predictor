f8503b9ab1fa18d6c845f1c57efd5eef0dd7b2af
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-11-24 01:45:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-model = pickle.load(open('iri.pkl', 'rb'))
+model = pickle.load(open('model_pickle.pkl', 'rb'))
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
@@ -19,7 +18,15 @@ def home():
     data2 = request.form['b']
     data3 = request.form['c']
     data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    data9 = request.form['i']
+    data10 = request.form['j']
+    data11= request.form['k']
+   
+    arr = np.array([[data1, data2, data3, data4,data5,data6,data7,data8,data9,data10,data11]])
     pred = model.predict(arr)
     return render_template('after.html', data=pred)
 
