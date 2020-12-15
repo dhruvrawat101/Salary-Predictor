4349778b1b4c8176762f5aeb82e474f2eebaa4a8
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-15 14:56:50 +0530

    updated model with home page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 import pickle
 import numpy as np
 
-model = pickle.load(open('model_pickle.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -14,21 +14,82 @@ def gh():
 
 @app.route('/predict', methods=['POST'])
 def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    data5 = request.form['e']
-    data6 = request.form['f']
-    data7 = request.form['g']
-    data8 = request.form['h']
-    data9 = request.form['i']
-    data10 = request.form['j']
-    data11= request.form['k']
+    data1 = request.form['rating']
+    data2 = request.form['age']
+    data3 = request.form['role1']
+    data4 = request.form['role2']
+    data5 = request.form['role3']
+    data6 = request.form['role4']
+    data7 = request.form['skill1']
+    data8 = request.form['skill2']
+    data9 = request.form['skill3']
+    data10 = request.form['skill4']
    
-    arr = np.array([[data1, data2, data3, data4,data5,data6,data7,data8,data9,data10,data11]])
+    if(data3=="Data Scientist"):
+        data3=1
+        data4=0
+        data5=0
+        data6=0
+    
+    elif(data2=="Data Analyst"):
+        data3=0
+        data4=1
+        data5=0
+        data6=0
+
+    elif(data3=="Data Engineering"):
+        data3=0
+        data4=0
+        data5=1
+        data6=0
+    
+    else:
+        data3=0
+        data4=0
+        data5=0
+        data6=1
+
+
+
+    for x in range(4):
+        if(data7=="python_job"):
+            count=1
+        if(data8=="excel_job"):
+            count=2
+        if(data9=="sql_job"):
+            count=3
+        if(data10=="tableau_job"):
+            count=4
+    
+    if(count==1):
+        data7=1
+        data8=0
+        data9=0
+        data10=0
+    if(count==2):
+        data7=1
+        data8=1
+        data9=0
+        data10=0
+
+if(count==3):
+        data7=1
+        data8=1
+        data9=1
+        data10=0
+
+if(count==4):
+        data7=1
+        data8=1
+        data9=1
+        data10=1
+
+
+    arr = np.array([[data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]])
     pred = model.predict(arr)
     return render_template('after.html', data=pred)
+
+
 
 #second way
 #return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
