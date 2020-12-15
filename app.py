19734ee7e7478ca7816b8df51dbddf768881a6ac
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-16 00:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import numpy as np
 
 model = pickle.load(open('model.pkl', 'rb'))
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder="template")
 
 
 @app.route('/')
