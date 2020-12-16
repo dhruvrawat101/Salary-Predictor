d752623fb3c6b1dd484714fa3c261f99fae2b1ef
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-17 00:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.get_data()]
+    int_features = [int(x) for x in request.form()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
