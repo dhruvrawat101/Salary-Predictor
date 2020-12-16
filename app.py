4bd93aae4209da09cd74ecf7838bce043a3d20f1
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-17 00:16:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,16 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
-    return render_template('after.html', data=output)
+        output = round(prediction[0], 2)
+        return render_template('after.html', data=output)
 
+    else:
+        return render_template(after.html', data=0)
 
 if __name__ == "__main__":
     app.run(debug=True)
