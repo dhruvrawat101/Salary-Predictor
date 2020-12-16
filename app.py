82dd08bd0ee97b2d95d66064973d86f1cc059eb0
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-17 00:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,100 +1,26 @@
-from flask import Flask, render_template,request
-import pickle
 import numpy as np
-
-model = pickle.load(open('model.pkl', 'rb'))
+from flask import Flask, request,render_template
+import pickle
 
 app = Flask(__name__)
-#app = Flask(__name__, template_folder="template")
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-def gh():
+def home():
     return render_template('home.html')
 
-
-@app.route('/predict', methods=['POST'])
-def home():
-    #data1 = request.get_data('rating')
-    #data2 = request.get_data('age')
-    #data3 = request.get_data('a')
-    #data4 = request.get_data('b')
-    #data5 = request.get_data('c')
-    #data6 = request.get_data('d')
-    #data7 = request.get_data('e')
-    #data8 = request.get_data('f')
-    #data9 = request.get_data('g')
-    #data10 =request.get_data('h')
-
-    int_features = [int(x) for x in request.form.values()]
-    arr = [np.array(int_features)]
-'''
-    if(data3=="DataScientist"):
-        data3=1
-        data4=0
-        data5=0
-        data6=0
-    
-    elif(data2=="DataAnalyst"):
-        data3=0
-        data4=1
-        data5=0
-        data6=0
-
-    elif(data3=="DataEngineering"):
-        data3=0
-        data4=0
-        data5=1
-        data6=0
-    
-    else:
-        data3=0
-        data4=0
-        data5=0
-        data6=1
-
-
-    count=0
-    for x in range(4):
-        if(data7=="on"):
-            count=1
-        if(data8=="on"):
-            count=2
-        if(data9=="on"):
-            count=3
-        if(data10=="on"):
-            count=4
-    
-    if(count==1):
-        data7=1
-        data8=0
-        data9=0
-        data10=0
-    
-    if(count==2):
-        data7=1
-        data8=1
-        data9=0
-        data10=0
-
-    if(count==3):
-        data7=1
-        data8=1
-        data9=1
-        data10=0
-
-    else:
-        data7=1
-        data8=1
-        data9=1
-        data10=1
-
-    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10]])
+@app.route('/predict',methods=['POST'])
+def predict():
     '''
-    pred = model.predict(arr)
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
     return render_template('after.html', data=pred)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
