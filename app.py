398d2bea7f6ca49847aff209bf47846d906ab818
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-17 18:52:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def predict():
 
     arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10]])
     pred = model.predict(arr)
-    pred = round(pred,2)
+    #pred = round(pred,2)
+     pred =  int(pred)
     return render_template('after.html', data=pred)
 
 if __name__ == "__main__":
